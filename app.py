a2fd458ef4c9e240d728ba5b155f5897345c32fe
d02898872-blip <d02898872@gmail.com>
2026-01-13 23:25:37 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 def detector(score):
+    if score is None:
+        return {"result": "Invalid"}
     return {"result": "AI" if score >= 50 else "Human"}
 
 @app.route("/detect", methods=["POST"])
